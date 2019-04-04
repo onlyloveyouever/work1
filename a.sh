71e049f9b40cfbb5ddb3000a31eba78f38471f74
sdpan <306465391@qq.com>
2019-04-04 13:54:08 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -152,17 +152,6 @@ hal config ci jenkins enable
 # Deploy Spinnaker to local VM
 sudo hal deploy apply
 
-run_util_script "jenkins/install_jenkins.sh" -jf "${vm_fqdn}" -al "${artifacts_location}" -st "${artifacts_location_sas_token}"
-
-run_util_script "jenkins/init-aptly-repo.sh" -vf "${vm_fqdn}" -rn "${repository_name}"
-
-run_util_script "jenkins/add-aptly-build-job.sh" -al "${artifacts_location}" -st "${artifacts_location_sas_token}"
-
-echo "Setting up initial user..."
-echo "jenkins.model.Jenkins.instance.securityRealm.createAccount(\"$jenkins_username\", \"$jenkins_password\")"  > addUser.groovy
-run_util_script "jenkins/run-cli-command.sh" -cif "addUser.groovy" -c "groovy ="
-rm "addUser.groovy"
-
 # Wait for Spinnaker services to be up before returning
 timeout=180
 echo "while !(nc -z localhost 8084) || !(nc -z localhost 9000); do sleep 1; done" | timeout $timeout bash
